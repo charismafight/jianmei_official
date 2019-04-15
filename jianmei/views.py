cf56ab06df8c8e6806345d187d777ef21013c55c
Lee <charismafight@hotmail.com>
2019-04-16 00:23:16 +0800

    add house page and some images

--- jianmei/views.py
+++ jianmei/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'jianmei/index.html', None)
+
+
+def hot_house(request):
+    return render(request, 'jianmei/hot_house.html', None)
--- jianmei_official/urls.py
+++ jianmei_official/urls.py
@@ -20,4 +20,5 @@ from jianmei import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index),
+    path('house/', views.hot_house)
 ]
